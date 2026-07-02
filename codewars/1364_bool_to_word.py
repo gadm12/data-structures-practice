b5cf8f1a89c25e7d84941ac7ff7b69f57df92a41
Mohamed Gad <gadm5336@gmail.com>
2026-07-01 21:19:32 -0500

    boolean to word

---
+++ codewars/1364_bool_to_word.py
@@ -0,0 +1,10 @@
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    if boolean == False:
+        return "No"
+
+
+print(bool_to_word("Yes"))
+
+print(bool_to_word("No"))
